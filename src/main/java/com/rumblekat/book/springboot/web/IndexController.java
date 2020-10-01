7772fba144567b05618054334e7ab421717fe7c4
RumbleKAT <reki318@naver.com>
2020-10-01 15:48:21 +0900

    add Login singleton Object

--- src/main/java/com/rumblekat/book/springboot/web/IndexController.java
+++ src/main/java/com/rumblekat/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.rumblekat.book.springboot.web;
 
+import com.rumblekat.book.springboot.config.auth.LoginUser;
 import com.rumblekat.book.springboot.config.auth.dto.SessionUser;
 import com.rumblekat.book.springboot.service.posts.PostsService;
 import com.rumblekat.book.springboot.web.dto.PostsResponseDto;
@@ -22,9 +23,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-       model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    public String index(Model model, @LoginUser SessionUser user){
+        model.addAttribute("posts",postsService.findAllDesc());
         if(user != null){
             model.addAttribute("userName",user.getName());
         }
